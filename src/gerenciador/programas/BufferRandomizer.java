d7c6a90246f773c36c1399bce7d315e6045cdf17
Nedehf <nedehf@edu.unifor.br>
2016-09-30 19:27:15 -0300

    Classes (vou ajeitar o Rowld)

--- src/gerenciador/programas/BufferRandomizer.java
+++ src/gerenciador/programas/BufferRandomizer.java
@@ -10,35 +10,54 @@ import gerenciador.loger.Log;
 
 public class BufferRandomizer {
 	private static ILog Log;
+
 	public static void main(String[] args) {
 
-		String containerId ;
+		String containerId;
+		Log = new Log();
+		int forty;
+		float four;
 
-		do{
+		do {
 			containerId = JOptionPane.showInputDialog("Id da tabela");
-			if(containerId == null) return;
-		}while(containerId.isEmpty());
-		
-		
-		try{
-			GerenciadorBuffer gb = new GerenciadorBuffer();
+			if (containerId == null)
+				return;
+		} while (containerId.isEmpty());
+
+		try {
+//			GerenciadorBuffer gb = new GerenciadorBuffer();
 			GerenciadorArquivos ga = new GerenciadorArquivos();
 			Arquivo a = ga.getArquivo(Byte.parseByte(containerId));
+
+			// Log = new LogLeituraTabela(a.getFile());
 			
-//			Log = new LogLeituraTabela(a.getFile());
-			Log = new Log();
-			Log.Write("Importando todos os blocos");
-			for(int i = 1; i < a.getBlocoControle().getProxBlocoLivre(); i++){
-//				Log.Write(ga.getBloco(a.getId(), i).toString());		
-				Log.Write("Bloco..." + ga.getBloco(a.getId(), i).getBlocoId()+"...Importado");
+			Log.Write("Verificando número de blocos");
+			System.out.println(a.getBlocoControle().getProxBlocoLivre()-1);
+			
+			Log.Write("Calculando 40% dos blocos");
+			four = 0.4f;
+			forty = (int) ((a.getBlocoControle().getProxBlocoLivre()-1)*four);
+			System.out.println(forty);
+
+			Log.Write("Importando os blocos");
+			for (int i = 1; i < forty; i++) {
+
+//				System.out.println(a.getBlocoControle().getProxBlocoLivre());
+//				Log.Write(ga.getBloco(a.getId(), i).toString());
+				
+				Log.Write("Bloco..." + ga.getBloco(a.getId(), i).getBlocoId() + "...Importado");
+				
+//				Log.Write("Importando as tuplas");
+//				Log.Write("\n");
+//				Log.Write("Tupla..." + ga.getBloco(a.getId(), i).getBlocoTupleId() + "...Importado");
+				
 //				gb.getBloco(ga.getBloco(a.getId(), i).getBlocoTupleId());
 			}
 			Log.Write("Blocos importados");
-			
-			
-		}catch(NumberFormatException e){
+
+		} catch (NumberFormatException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 }
---
+++ src/gerenciador/programas/ListaRowID.java
@@ -0,0 +1,60 @@
+package gerenciador.programas;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import gerenciador.GerenciadorBuffer;
+import gerenciador.RowId;
+
+public class ListaRowID {
+
+	public static void main(String[] args) {
+		final String arquivo = "listaID.txt";
+		GerenciadorBuffer bm = new GerenciadorBuffer();
+
+		// public ArrayList<RowId> listar[] {
+		ArrayList<RowId> listaID = new ArrayList<RowId>();
+
+		FileReader fr = null;
+		BufferedReader br = null;
+
+		try {
+			fr = new FileReader(arquivo);
+			br = new BufferedReader(fr);
+
+			String linha;
+			while ((linha = br.readLine()) != null) {
+				String[] dados = linha.split(".");
+				RowId Ri = new RowId(Integer.parseInt(dados[0]), Integer.parseInt(dados[1]),
+						Integer.parseInt(dados[2]));
+//				dado do container no rowid tipo byte
+				listaID.add(Ri);
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+				fr.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// return listaID;
+		// }
+
+		for (RowId id : listaID) {
+
+			bm.getBloco(id);
+//colocar os log.write no getbloco
+		}
+
+	}
+
+}
